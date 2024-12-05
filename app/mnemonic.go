8af983ff48f7d27d420b8d12dc1715f6b0651979
Dmitry Sidorov <viwet42@gmail.com>
2024-12-05 19:59:20 +0100

    feat(refactor): add GenerateMnemnoic function

--- app/mnemonic.go
+++ app/mnemonic.go
@@ -1,6 +1,59 @@
 package app
 
-import "strings"
+import (
+	"strings"
+
+	bip39 "github.com/viwet/GoBIP39"
+	"github.com/viwet/GoBIP39/words"
+)
+
+// GenerateMnemonic generates new seed phrase
+func GenerateMnemonic(cfg *MnemonicConfig) ([]string, words.List, error) {
+	entropy, err := bip39.NewEntropy(cfg.Bitlen)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	list := languageFromMnemonicConfig(cfg)
+	mnemonic, err := bip39.ExtractMnemonic(entropy, list)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := bip39.ValidateMnemonic(mnemonic, list); err != nil {
+		return nil, nil, err
+	}
+
+	return mnemonic, list, nil
+}
+
+func languageFromMnemonicConfig(cfg *MnemonicConfig) words.List {
+	switch strings.ReplaceAll(strings.ToLower(cfg.Language), " ", "") {
+	case "english":
+		return words.English
+	case "chinesesimplified":
+		return words.ChineseSimplified
+	case "chinesetraditional":
+		return words.ChineseTraditional
+	case "czech":
+		return words.Czech
+	case "french":
+		return words.French
+	case "italian":
+		return words.Italian
+	case "japanese":
+		return words.Japanese
+	case "korean":
+		return words.Korean
+	case "portuguese":
+		return words.Portuguese
+	case "spanish":
+		return words.Spanish
+	default:
+		// Config is assumed validated
+		panic(ErrInvalidMnemonicLanguage)
+	}
+}
 
 var allowedLanguagesNames = [10]string{
 	"english",
