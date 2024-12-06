82fd4d05eff82f9cdcc93562d749d5eafa066f10
Dmitry Sidorov <viwet42@gmail.com>
2024-12-06 14:22:37 +0100

    test(refactor): test BLSToExecutionConfig validation function

--- app/validation_test.go
+++ app/validation_test.go
@@ -17,7 +17,7 @@ const (
 	deadAddress2 = "0xdeaddeaddeaddeaddeaddeaddeaddeaddead0002"
 )
 
-func Test_ensureDepositConfigIsValid(t *testing.T) {
+func Test_EnsureDepositConfigIsValid(t *testing.T) {
 	tests := []struct {
 		name       string
 		makeConfig func(t *testing.T) *DepositConfig
@@ -293,6 +293,312 @@ func Test_ensureDepositConfigIsValid(t *testing.T) {
 				}
 			} else if test.error != nil {
 				t.Fatalf("want error %v, but got nil", test.error)
+			}
+		})
+	}
+}
+
+func Test_EnsureBLSToExecutionConfigIsValid(t *testing.T) {
+	tests := []struct {
+		name       string
+		makeConfig func(t *testing.T) *BLSToExecutionConfig
+		checkError func(t *testing.T, err error)
+	}{
+		{
+			name: "valid config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				genesisForkVersion, err := hex.DecodeString(deadGenesisForkVersion)
+				if err != nil {
+					t.Fatal(err)
+				}
+				genesisValidatorsRoot, err := hex.DecodeString(deadGenesisValidatorsRoot)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				var (
+					dead0 Address
+					dead1 Address
+					dead2 Address
+				)
+
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+				if err := dead1.FromHex(deadAddress1); err != nil {
+					t.Fatal(err)
+				}
+				if err := dead2.FromHex(deadAddress2); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					Config: &Config{
+						Number: 2,
+						ChainConfig: &config.ChainConfig{
+							Name:                  "devnet",
+							GenesisForkVersion:    genesisForkVersion,
+							GenesisValidatorsRoot: genesisValidatorsRoot,
+						},
+						MnemonicConfig: &MnemonicConfig{
+							Language: "english",
+							Bitlen:   256,
+						},
+						Directory: "./validators_data",
+					},
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{
+							0: 0,
+							1: 1,
+						},
+					},
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						Default: dead0,
+						IndexedConfig: IndexedConfig[Address]{
+							Config: map[uint32]Address{
+								1: dead1,
+							},
+						},
+					},
+				}
+			},
+			checkError: nil,
+		},
+		{
+			name: "invalid config: invalid withdrawal addresses config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{
+							0: 0,
+						},
+					},
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						IndexedConfig: IndexedConfig[Address]{
+							Config: map[uint32]Address{
+								1: dead0,
+							},
+						},
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				want := fmt.Errorf(
+					"invalid withdrawal addresses config: key index should be between %d and %d, but got %d",
+					0,
+					1,
+					1,
+				)
+
+				if got == nil {
+					t.Fatalf("want error %v, but got nil", want)
+				}
+
+				if want.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want, got)
+				}
+			},
+		},
+		{
+			name: "invalid config: invalid validator indices config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					Config: &Config{
+						Number: 3,
+					},
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{
+							0: 0,
+							1: 1,
+							2: 2,
+						},
+					},
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						IndexedConfig: IndexedConfig[Address]{
+							Config: map[uint32]Address{
+								0: dead0,
+								1: dead0,
+							},
+						},
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				want := fmt.Errorf("no withdrawal addresses for key indices: %v", []uint32{2})
+
+				if got == nil {
+					t.Fatalf("want error %v, but got nil", want)
+				}
+
+				if want.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want, got)
+				}
+			},
+		},
+		{
+			name: "invalid config: invalid withdrawal addresses config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					Config: &Config{
+						Number: 3,
+					},
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{
+							0: 0,
+							2: 2,
+						},
+					},
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						Default: dead0,
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				want := fmt.Errorf("no validator indices for key indices: %v", []uint32{1})
+
+				if got == nil {
+					t.Fatalf("want error %v, but got nil", want)
+				}
+
+				if want.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want, got)
+				}
+			},
+		},
+		{
+			name: "invalid config: no withdrawal addresses config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{0: 0},
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				want := ErrNoWithdrawalAddresses
+
+				if got == nil {
+					t.Fatalf("want error %v, but got nil", want)
+				}
+
+				if want.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want, got)
+				}
+			},
+		},
+		{
+			name: "invalid config: no validator indices config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						Default: dead0,
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				want := ErrNoValidatorIndices
+
+				if got == nil {
+					t.Fatalf("want error %v, but got nil", want)
+				}
+
+				if want.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want, got)
+				}
+			},
+		},
+		{
+			name: "invalid config: no validator indices config",
+			makeConfig: func(t *testing.T) *BLSToExecutionConfig {
+				var dead0 Address
+				if err := dead0.FromHex(deadAddress0); err != nil {
+					t.Fatal(err)
+				}
+
+				return &BLSToExecutionConfig{
+					Config: &Config{
+						Number: 3,
+					},
+					ValidatorIndices: &IndexedConfig[uint64]{
+						Config: map[uint32]uint64{
+							0: 0,
+							1: 0,
+							2: 2,
+						},
+					},
+					WithdrawalAddresses: &IndexedConfigWithDefault[Address]{
+						Default: dead0,
+					},
+				}
+			},
+			checkError: func(t *testing.T, got error) {
+				var dead1 Address
+				if err := dead1.FromHex(deadAddress1); err != nil {
+					t.Fatal(err)
+				}
+
+				want1 := fmt.Errorf(
+					"invalid validator indices config: %d and %d have the same validator index %d",
+					0,
+					1,
+					0,
+				)
+				want2 := fmt.Errorf(
+					"invalid validator indices config: %d and %d have the same validator index %d",
+					1,
+					0,
+					0,
+				)
+
+				if got == nil {
+					t.Fatalf("want error %v or %v, but got nil", want1, want2)
+				}
+
+				if want1.Error() != got.Error() && want2.Error() != got.Error() {
+					t.Fatalf("invalid error\nwant: %v\ngot:  %v", want1, got)
+				}
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			config := test.makeConfig(t)
+			err := EnsureBLSToExecutionConfigIsValid(config)
+			if err != nil {
+				if test.checkError == nil {
+					t.Fatalf("want no error, but got %v", err)
+				}
+				test.checkError(t, err)
+			} else {
+				if test.checkError != nil {
+					t.Fatal("want error, but got nil")
+				}
 			}
 		})
 	}
