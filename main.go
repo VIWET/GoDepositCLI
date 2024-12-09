111af5a98337f6d583ecb27f56be2df3b86ddd8b
Dmitry Sidorov <viwet42@gmail.com>
2024-12-09 15:13:34 +0100

    feat(refactor): remove cli package

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/viwet/GoDepositCLI/cli"
-)
-
 func main() {
-	app := cli.NewApp()
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
 }
